dcf6b54ea1884e79371bdec16a14c6b97e910d3b
AbdalllahEisa <89872569+AbdalllahEisa@users.noreply.github.com>
2021-09-27 09:30:53 +0200

    Update RestControllerApi.java

--- Apis/src/main/java/com/example/Apis/RestControllerApi.java
+++ Apis/src/main/java/com/example/Apis/RestControllerApi.java
@@ -6,33 +6,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class RestControllerApi {
-    private body getBodyJson(String jsonBodyDTO) throws IOException {
+    private body[] getBodyJson(String jsonBodyDTO) throws IOException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
         return new ObjectMapper()
                 .setDateFormat(simpleDateFormat)
-                .readValue(jsonBodyDTO, body.class);
+                .readValue(jsonBodyDTO, body[].class);
     }
 
     @RequestMapping("/ticks")
     @Cacheable(value = "ticks")
+
     public Object getBodyDto()
             throws IOException {
-        String BodyDto = "{\n" +
+        String BodyDto = "[{\n" +
                 "\n" +
                 "\n" +
                 "  \"instrument\": \"EURUSD\",\n" +
-                "  \"price\": 2.333,\n" +
+                "  \"price\": 1.333,\n" +
                 "  \"timestamp\": 1478192204000\n" +
                 "\n" +
                 "\n" +
@@ -44,17 +41,16 @@ public class RestControllerApi {
                 "  \"timestamp\": 1478192204000\n" +
                 "\n" +
                 "\n" +
-                "}";
+                "}]";
 
         LocalDateTime timeing = LocalDateTime.now();
         long sec = 60 - timeing.getSecond();
-        body i = new body(getBodyJson(BodyDto).getTimestamp(), getBodyJson(BodyDto).getPrice(), getBodyJson(BodyDto).getInstrument());
 
         double r = 0;
         if (sec >= 0) {
 
 
-            return i.getPrice();
+            return getBodyJson(BodyDto)[0].getPrice();
         } else {
 
 
@@ -71,11 +67,11 @@ public class RestControllerApi {
     @ResponseBody
     @Cacheable(value = "statistics")
     public ResponseEntity<stastics> stasttics() throws IOException {
-        String BodyDto = "{\n" +
+        String BodyDto = "[{\n" +
                 "\n" +
                 "\n" +
                 "  \"instrument\": \"EURUSD\",\n" +
-                "  \"price\": 2.333,\n" +
+                "  \"price\": 1.333,\n" +
                 "  \"timestamp\": 1478192204000\n" +
                 "\n" +
                 "\n" +
@@ -87,58 +83,59 @@ public class RestControllerApi {
                 "  \"timestamp\": 1478192204000\n" +
                 "\n" +
                 "\n" +
-                "}";
+                "}]";
         double r = 0;
-        int count = 0;
+        long count = 0;
         double avg = 0;
         double u = 0;
         double max = 0;
         double min = 0;
         LocalDateTime timeing = LocalDateTime.now();
         long sec = 60 - timeing.getSecond();
-        HashMap<String, body> y = new HashMap<>();
-        y.put("body", getBodyJson(BodyDto));
-        body i = new body(getBodyJson(BodyDto).getTimestamp(), getBodyJson(BodyDto).getPrice(), getBodyJson(BodyDto).getInstrument());
+        HashMap<String, Double> y = new HashMap<>();
+
+        Set<Double> mapSet=new HashSet<>();
+for(body t: getBodyJson(BodyDto)) {
+    
+    mapSet.add(t.getPrice());
+}
+
+
+
 
         if (sec >= 0) {
 
 
-            y.put("body", getBodyJson(BodyDto));
 
 
-            for (Map.Entry h : y.entrySet()) {
 
-                count++;
-                u += i.getPrice();
 
+
+            count++;
+
+
+
+
+
+
+
+            max =Collections.max(mapSet);
+            min=Collections.min(mapSet);
+            Iterator<Double> looping = mapSet.iterator();
+
+            count=   mapSet.stream().count();
+
+
+            while(looping.hasNext()) {
+                avg = looping.next() /count;
             }
-            avg = u / count;
-
-            for (int j = 0; j < y.size(); j++) {
-                if (y.get(0).getPrice() > y.get((j - 1) + 1).getPrice()) {
-                    max = y.get(0).getPrice();
-
-                } else {
-                    max = y.get((j - 1) + 1).getPrice();
-
-                }
-            }
-
-
-            for (int j = 0; j < y.size(); j++) {
-                if (y.get(0).getPrice() > y.get((j - 1) + 1).getPrice()) {
-                    min = y.get((j - 1) + 1).getPrice();
-
-                } else {
-
-                    min = y.get(0).getPrice();
-
-                }
-            }
-
         }
 
+
+
+
         return new ResponseEntity<stastics>(new stastics(count, avg, max, min), HttpStatus.OK);
+
 
 
     }
@@ -146,11 +143,11 @@ public class RestControllerApi {
     @GetMapping(" /statistics/{instrument_identifier}")
     @ResponseBody
     public ResponseEntity<stastics> stastticsidentifier(@PathVariable("instrument_identifier") String instrument) throws IOException {
-        String BodyDto = "{\n" +
+        String BodyDto = "[{\n" +
                 "\n" +
                 "\n" +
                 "  \"instrument\": \"EURUSD\",\n" +
-                "  \"price\": 2.333,\n" +
+                "  \"price\": 1.333,\n" +
                 "  \"timestamp\": 1478192204000\n" +
                 "\n" +
                 "\n" +
@@ -162,55 +159,59 @@ public class RestControllerApi {
                 "  \"timestamp\": 1478192204000\n" +
                 "\n" +
                 "\n" +
-                "}";
-        double r = 0;
-        int count = 0;
+                "}]";
+
+        long count = 0;
         double avg = 0;
         double u = 0;
         double max = 0;
         double min = 0;
         LocalDateTime timeing = LocalDateTime.now();
         long sec = 60 - timeing.getSecond();
-        HashMap<String, body> y = new HashMap<>();
-        body i = new body(getBodyJson(BodyDto).getTimestamp(), getBodyJson(BodyDto).getPrice(), getBodyJson(BodyDto).getInstrument());
-        y.put("body", getBodyJson(BodyDto));
+        HashMap<String, Double> y = new HashMap<>();
+        Set<Double> mapSet=new HashSet<>();
+
+
+
+        for(body t: getBodyJson(BodyDto)) {
+            mapSet.add(t.getPrice());
+        }
+
 
 
         if (sec >= 0) {
 
 
-            for (Map.Entry h : y.entrySet()) {
 
-                count++;
-                u += i.getPrice();
 
+
+
+
+            count++;
+
+
+
+
+
+
+
+            max =Collections.max(mapSet);
+            min=Collections.min(mapSet);
+            Iterator<Double> looping = mapSet.iterator();
+
+            count=   mapSet.stream().count();
+
+
+            while(looping.hasNext()) {
+                avg = looping.next() /count;
             }
-            avg = u / count;
-
-            for (int j = 0; j < y.size(); j++) {
-                if (y.get(0).getPrice() > y.get((j - 1) + 1).getPrice()) {
-                    max = y.get(0).getPrice();
-
-                } else {
-                    max = y.get((j - 1) + 1).getPrice();
-
-                }
-            }
-
-
-            for (int j = 0; j < y.size(); j++) {
-                if (y.get(0).getPrice() > y.get((j - 1) + 1).getPrice()) {
-                    min = y.get((j - 1) + 1).getPrice();
-
-                } else {
-
-                    min = y.get(0).getPrice();
-
-                }
-            }
-
         }
+
+
+
+
         return new ResponseEntity<stastics>(new stastics(count, avg, max, min), HttpStatus.OK);
+
 
 
     }
