a72683d80867f46faf218e61fdf1a3108f4a7f15
AbdalllahEisa <89872569+AbdalllahEisa@users.noreply.github.com>
2021-10-01 01:37:39 +0200

    Update body.java

--- Apis/src/main/java/com/example/Apis/body.java
+++ Apis/src/main/java/com/example/Apis/body.java
@@ -3,18 +3,27 @@ package com.example.Apis;
 import java.sql.Timestamp;
 
 public class body {
-
+int id;
     Timestamp timestamp;
     double price;
     String instrument;
 
-    public body(Timestamp timestamp, double price, String instrument) {
+    public body(int id,Timestamp timestamp, double price, String instrument) {
         this.timestamp = timestamp;
+        this.id = id;
         this.price = price;
         this.instrument = instrument;
     }
     public body() {
       }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public Timestamp getTimestamp() {
         return timestamp;
